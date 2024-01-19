8e8c016a4437e385c14f2e313d8fe749dc052fbe
vstjal001 <vstjal001@cs.uct.ac.za>
2024-01-19 20:47:38 +0200

    Add image domain to Next.js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        // https://image.tmdb.org/t/p/w600_and_h900_bestv2/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg
+        domains: ['image.tmdb.org'],
+    },
+}
 
 module.exports = nextConfig
