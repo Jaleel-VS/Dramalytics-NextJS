d9ba3ce5936a18c0db8463486f61444888daadac
vstjal001 <vstjal001@cs.uct.ac.za>
2024-01-19 18:21:22 +0200

    Add search functionality and display search results

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,73 @@
+"use client";
 import Image from 'next/image'
+import { useState } from 'react';
+import SearchBar from './_components/SearchBar';
+import SearchResult from './_components/SearchResult';
+import { SearchResultType } from '../types/types';
+import React from 'react';
+
+
 
 export default function Home() {
-  return (
-    <main className="container w-full mx-auto">
-      <h1>Home</h1>
-      {/* button + link to about page */}
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        <a href="/about">About</a>
-      </button>
+  const results: SearchResultType[] = [
+    {
+      tmdb_id: "101",
+      title: "Space Odyssey",
+      year: "2021",
+      poster: "https://example.com/posters/space-odyssey.jpg",
+      voteAverage: 8.5
+    },
+    {
+      tmdb_id: "102",
+      title: "The Great Adventure",
+      year: "2019",
+      poster: "https://example.com/posters/great-adventure.jpg",
+      voteAverage: 7.9
+    },
+    {
+      tmdb_id: "103",
+      title: "Mystery of the Depths",
+      year: "2023",
+      poster: "https://example.com/posters/mystery-depths.jpg",
+      voteAverage: 6.8
+    },
+    {
+      tmdb_id: "104",
+      title: "Future World",
+      year: "2022",
+      poster: "https://example.com/posters/future-world.jpg",
+      voteAverage: 9.1
+    },
+    {
+      tmdb_id: "105",
+      title: "Legends of Yesterday",
+      year: "2020",
+      poster: "https://example.com/posters/legends-yesterday.jpg",
+      voteAverage: 7.4
+    }
+  ];
+  
+  const [searchQuery, setSearchQuery] = useState('');
+  const [displayResults, setDisplayResults] = useState(false);
 
-    </main>
+  const handleSearchClick = (query: string) => {
+    setSearchQuery(query);
+    setDisplayResults(true);
+  };
+
+  const filteredResults = results.filter(result => {
+    return result.title.toLowerCase().includes(searchQuery.toLowerCase());
+  }
+  );
+
+  return (
+    <div className="container mx-auto my-8">
+      <SearchBar onSearchClick={handleSearchClick} />
+      <div>
+        {displayResults && filteredResults.map(result => (
+          <SearchResult key={result.tmdb_id} {...result} />
+        ))}
+      </div>
+    </div>
   )
 }
